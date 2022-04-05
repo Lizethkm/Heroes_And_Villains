e50783b78079508ff338fe98fc7a250363fc3e60
Lizeth <mendezlizeth084@gmail.com>
2022-04-05 08:21:14 -0700

    Imported Heroes_Villains Postman Collection + Created Supers Model in Supers App

--- supers/models.py
+++ supers/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
+from super_types.models import SuperType
+
 # Create your models here.
-class Hero(models.Model):
-    pass
+class Supers(models.Model):
+    name= models.CharField(max_length=250)
+    alter_ego= models.CharField(max_length=250)
+    primary_ability= models.CharField(max_length=250)
+    second_ability= models.CharField(max_length=250)
+    catchphrase= models.CharField(max_length=250)
+    super_type= models.ForeignKey(SuperType, on_delete=None)
 
