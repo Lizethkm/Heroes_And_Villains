38c34062f5a516c0eeee9295884ca7c6d2c8b25e
Lizeth <mendezlizeth084@gmail.com>
2022-04-06 20:07:34 -0700

    Created Get_All Function in Views File in Super_Types App

--- super_types/views.py
+++ super_types/views.py
@@ -1,3 +1,16 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import SuperTypeSerializer
+from .models import SuperType
+
 
 # Create your views here.
+@api_view(['GET'])
+
+def list_super_types(request):
+    super_type= SuperType.objects.all()
+    serializer= SuperTypeSerializer(super_type, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
